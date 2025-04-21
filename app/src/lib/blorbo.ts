1f8bc36743a2ac936e348c94bd2c1276fcc3b65a
Red <92976307+TheRedDragon7913@users.noreply.github.com>
2025-04-20 22:58:09 -0400

    Update blorbo.ts
    I am gonna be so real here I have no fucking clue what I'm doing lmfao

--- app/src/lib/blorbo.ts
+++ app/src/lib/blorbo.ts
@@ -8,24 +8,33 @@ export interface CheckboxTrait {
   selected: boolean;
 }
 
-export interface Subclass extends CheckboxTrait {}
-export interface Addon extends CheckboxTrait {}
-export interface Evoke extends CheckboxTrait {}
-export interface Want extends CheckboxTrait {}
-
 export interface SliderTrait {
   negativePole: string;
   positivePole: string;
   amount: number | null;
 }
 
+export interface MindsetOutset extends SliderTrait {}
+export interface MindsetPresent extends SliderTrait {}
+export interface MindsetEndgame extends SliderTrait {}
+export interface CircumstanceOutset extends SliderTrait {}
+export interface CircumstancePresent extends SliderTrait {}
+export interface CircumstanceEndgame extends SliderTrait {}
+export interface growthSlidersPast extends SliderTrait {}
+export interface growthSlidersFuture extends SliderTrait {}
+
+
 export interface BlorboSettings {
   name: string;
-  imageDataUrl: string | null;
-  traits: QuantitativeTrait[];
-  subclasses: Subclass[];
-  addons: Addon[];
-  sliders: SliderTrait[];
-  evokes: Evoke[];
-  wantThemToHave: Want[];
+
+  mindsetOutset: MindsetOutset[];
+  mindsetPresent: MindsetPresent[];
+  mindsetEndgame: MindsetEndgame[];
+  circumstanceOutset: CircumstanceOutset[];
+  circumstancePresent: CircumstancePresent[];
+  circumstanceEndgame: CircumstanceEndgame[];
+  growthChangesPast: CheckboxTrait[];
+  growthChangesFuture: CheckboxTrait[];
+  growthSlidersPast: GrowthSlidersPast[];
+  growthSlidersFuture: GrowthSlidersFuture[];
 }
